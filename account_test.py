ea005e10d6bdbeb21420980c10d2d7b27f48cb82
SharonMigot <migotsharon@gmail.com>
2020-10-11 23:23:33 +0300

    corrected an error in account_test.py

--- account_test.py
+++ account_test.py
@@ -5,13 +5,13 @@ from account import Account
 class TestAccount(unittest.TestCase):
 
     def setUp(self):
-        self.new_account = Account("Anna","Adhiambo","0758579812","annaadhiambo01@gmail.com","AnnaAdhiambo","12345")
+        self.new_account = Account("Migot","Sharon","0790437491","migotsharon@gmail.com","MigotSharon","12345")
     def test_init(self):
-        self.assertEqual(self.new_account.first_name,"Anna")
-        self.assertEqual(self.new_account.last_name,"Adhiambo")
-        self.assertEqual(self.new_account.phone_number,"0758579812")
-        self.assertEqual(self.new_account.email,"annaadhiambo01@gmail.com")
-        self.assertEqual(self.new_account.username,"AnnaAdhiambo")
+        self.assertEqual(self.new_account.first_name,"Migot")
+        self.assertEqual(self.new_account.last_name,"Sharon")
+        self.assertEqual(self.new_account.phone_number,"0790437491")
+        self.assertEqual(self.new_account.email,"migotsharon@gmail.com")
+        self.assertEqual(self.new_account.username,"MigotSharon")
         self.assertEqual(self.new_account.password,"12345")
 
     def test_save_account(self):
@@ -26,7 +26,7 @@ class TestAccount(unittest.TestCase):
         Method to check if we can save multiple account objects to our account_list
         '''
         self.new_account.save_account()
-        test_account = Account ("Anna","Adhiambo","0758579812","annaadhiambo01@gmail.com","AnnaAdhiambo","12345") 
+        test_account = Account ("Migot","Sharon","0790437491","migotsharon@gmail.com","MigotSharon","12345") 
         test_account.save_account()
         self.assertEqual(len(Account.account_list),2)  
 
